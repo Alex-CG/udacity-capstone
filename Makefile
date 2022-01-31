823ff003454b825f12ec23314349a0c8e2bdf1d5
Alex Contreras <a.contrerasgranados@gmail.com>
2022-01-30 22:36:05 -0600

    add config

--- Makefile
+++ Makefile
@@ -19,7 +19,8 @@ test:
 lint:
 	# See local hadolint install instructions:   https://github.com/hadolint/hadolint
 	# This is linter for Dockerfiles
-	./hadolint Dockerfile
+	./hadolint deploy/blue/Dockerfile
+	./hadolint deploy/green/Dockerfile
 
 	# This is a linter for Python source code linter: https://www.pylint.org/
 	# This should be run from inside a virtualenv
--- deploy/blue/run_docker.sh
+++ deploy/blue/run_docker.sh
@@ -2,11 +2,14 @@
 
 ## Complete the following steps to get Docker running locally
 
+# Step 1:
 # Build image and add a descriptive tag
-docker build --tag=alexcgdocker/mlapp .
+docker build --tag=alexcgdocker/ml-app-capstone-blue .
 
+# Step 2: 
 # List docker images
 docker image ls
 
+# Step 3: 
 # Run flask app
-docker run -p 8000:80 alexcgdocker/mlapp
+docker run -p 8000:80 alexcgdocker/ml-app-capstone-blue
